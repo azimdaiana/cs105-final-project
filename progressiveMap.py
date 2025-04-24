f53ae24e76747dbeab8c5d38e42265ddc0de5bad
mdoliveira <mdoliveira@brynmawr.edu>
2025-04-24 13:17:56 -0400

    progressive map, progressive map function

--- progressiveMap.py
+++ progressiveMap.py
@@ -1,5 +1,6 @@
 from loadingMaps import load_map
 from typing import List
+from movement_functions import getCurrentLocation
 
 #prints map when given input "print" and "map(number)" as parameter values
 def print_map(i):
@@ -15,13 +16,12 @@ def print_map(i):
         return load_map("map5.txt")
 
 print(print_map(input()))
-#
-# def print_map(i)->List[int]:
-#     m = []
-#     if 'print' and 'map1' in i:
-#         m = load_map('map1.txt')
-#     else:
-#         m = []
-#     return m
-#
-# print_map("print map1")
+
+#returns map with progess ('7' where player has been) when given current map configuration
+def progressive_map(c_map):
+    m = print_map(c_map)
+    l = getCurrentLocation()
+    m[l[0]][l[1]] = '7'
+    return m
+
+progressive_map("print map1")
