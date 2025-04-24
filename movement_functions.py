fac25b152d3acc3e1b8d1d53ade48f15630d8777
gabernathy <gabernathy>
2025-04-24 16:48:32 -0400

    movement functions wip

--- movement_functions.py
+++ movement_functions.py
@@ -14,29 +14,28 @@ def getCurrentLocation() -> tuple:
 print(getCurrentLocation())
 
 
-def setLocation(x: int, y: int) -> bool
+def setLocation(x: int, y: int) -> bool:
     precondition(x == player_x or y == player_y)
-    location: tuple = (player_x, player_y)
     if x > player_x:
-        if canGoEast():
+        if canGoEast(x):
             goEast(abs(player_x - x))
             print ("Moved " + abs(player_x - x) + " units east.")
         else:
             print("Cannot go east.")
     if x < player_x:
-        if canGoWest():
+        if canGoWest(x):
             goWest(abs(player_x - x))
             print("Moved " + abs(player_x - x) + " units west.")
         else:
             print("Cannot go west.")
     if y > player_y:
-        if canGoNorth():
+        if canGoNorth(y):
             goNorth(abs(player_y - y))
             print("Moved " + abs(player_y - y) + " units north.")
         else:
             print("Cannot go north.")
     if y < player_y:
-        if canGoSouth():
+        if canGoSouth(y):
             goSouth(abs(player_y - y))
             print("Moved " + abs(player_y - y) + " units south.")
         else:
@@ -44,29 +43,47 @@ def setLocation(x: int, y: int) -> bool
 
 
 def canGoEast(x: int) -> bool:
-    if map[i][j] == 1:
-        return True
-    else:
+    if map[player_y][x] == 0:
         return False
+    else:
+        return True
 
 
-def canGoWest() -> bool:
+def canGoWest(x: int) -> bool:
+    if map[player_y][x] == 0:
+        return False
+    else:
+        return True
 
+def canGoNorth(y: int) -> bool:
+    if map[y][player_x] == 0:
+        return False
+    else:
+        return True
 
-def canGoNorth() -> bool:
-
-
-def canGoSouth() -> bool:
+def canGoSouth(y: int) -> bool:
+    if map[y][player_x] == 0:
+        return False
+    else:
+        return True
 
 
 def goEast(x: int):
+    for i in range(abs(player_x - x)):
+        map[player_y][player_x + i] = 7
     player_x += x
 
 def goWest(x: int):
+    for i in range(abs(player_x - x)):
+        map[player_y][player_x - i] = 7
     player_x -= x
 
 def goNorth(y: int):
+    for i in range(abs(player_y - y)):
+        map[player_y + i][player_x] = 7
     player_y += y
 
 def goSouth(y: int):
+    for i in range(abs(player_y - y)):
+        map[player_y - i][player_x] = 7
     player_y -= y
