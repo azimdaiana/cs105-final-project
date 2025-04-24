3a50dc8ff5d342c02aac5df32da341ee69fcbc5d
mdoliveira <mdoliveira@brynmawr.edu>
2025-04-24 12:31:39 -0400

    progressive map, print map

--- progressiveMap.py
+++ progressiveMap.py
@@ -1,11 +1,27 @@
-
+from loadingMaps import load_map
+from typing import List
 #printing map when player inputs "print map"
-def print_map():
-    p = "print map"
-    i = input()
-    #map 1 is hardcoded into the map function, needs to be replaced with global variable
-    m = open('map1.txt','r')
-    if p == i:
-        print(m.read())
 
+def print_map(i):
+    if "print" and "map1" in i:
+        return load_map("map1.txt")
+    if "print" and "map2" in i:
+        return load_map("map2.txt")
+    if "print" and "map3" in i:
+        return load_map("map3.txt")
+    if "print" and "map4" in i:
+        return load_map("map4.txt")
+    if "print" and "map5" in i:
+        return load_map("map5.txt")
 
+print(print_map("print map1"))
+#
+# def print_map(i)->List[int]:
+#     m = []
+#     if 'print' and 'map1' in i:
+#         m = load_map('map1.txt')
+#     else:
+#         m = []
+#     return m
+#
+# print_map("print map1")
