8cc1901e8f605f68369e883c6370e73eb6139a28
mdoliveira <mdoliveira@brynmawr.edu>
2025-04-24 12:43:01 -0400

    progressive map, print map

--- progressiveMap.py
+++ progressiveMap.py
@@ -1,20 +1,20 @@
 from loadingMaps import load_map
 from typing import List
-#printing map when player inputs "print map"
 
+#prints map when given input "print" and "map(number)" as parameter values
 def print_map(i):
     if "print" and "map1" in i:
         return load_map("map1.txt")
-    if "print" and "map2" in i:
+    elif "print" and "map2" in i:
         return load_map("map2.txt")
-    if "print" and "map3" in i:
+    elif "print" and "map3" in i:
         return load_map("map3.txt")
-    if "print" and "map4" in i:
+    elif "print" and "map4" in i:
         return load_map("map4.txt")
-    if "print" and "map5" in i:
+    elif "print" and "map5" in i:
         return load_map("map5.txt")
 
-print(print_map("print map1"))
+print(print_map(input()))
 #
 # def print_map(i)->List[int]:
 #     m = []
